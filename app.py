9ff96aa428d1bf778a58032c392c5f45ca56ffac
Varad <varad.b@somaiya.edu>
2026-08-12 13:44:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,46 +6,41 @@ from sklearn.model_selection import train_test_split
 
 app = Flask(__name__)
 
-df = pd.read_csv('diabetes.csv')
-X = df[['Pregnancies','BloodPressure','SkinThickness','Insulin','BMI','Glucose','DiabetesPedigreeFunction','Age']]
-y = df['Outcome']
+df = pd.read_csv('heatwave.csv')
+X = df[['MaxTemp','MinTemp','Humidity','WindSpeed','Pressure','HistAvgTemp']]
+y = df['Heatwave']
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 model = LinearRegression()
 model.fit(X_train, y_train)
-
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
         input_features = [
-            float(request.form.get("Pregnancies")),
-            float(request.form.get("Glucose")),
-            float(request.form.get("BloodPressure")),
-            float(request.form.get("SkinThickness")),
-            float(request.form.get("Insulin")),
-            float(request.form.get("BMI")),
-            float(request.form.get("DiabetesPedigreeFunction")),
-            float(request.form.get("Age")),
+            float(request.form.get("MaxTemp")),
+            float(request.form.get("MinTemp")),
+            float(request.form.get("Humidity")),
+            float(request.form.get("WindSpeed")),
+            float(request.form.get("Pressure")),
+            float(request.form.get("HistAvgTemp")),
         ]
-
         prediction = model.predict([input_features])[0]
-        if prediction <= 0.25 : 
-            pred = 'low'
-        elif prediction >= 0.25 and prediction <= 50 : 
-            pred  = 'average'
-        else : 
-            pred = 'high'
+
+        if prediction <= 0.25:
+            pred = 'Low Risk'
+        elif prediction >= 0.25 and prediction <= 0.6:
+            pred = 'Moderate Risk'
+        else:
+            pred = 'High Risk — Heatwave Likely'
 
         return render_template("index.html", prediction_text=pred)
-
     except:
         return render_template("index.html", prediction_text="Error: Please enter valid numeric values.")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
